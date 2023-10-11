2717fac2bdc709b62b6be200e0667fb721dac706
ystepane <julia.v.stepanenko@brown.edu>
2023-10-11 15:38:58 -0400

    MockData upload

---
+++ src/components/MockData.tsx
@@ -0,0 +1,8 @@
+const CSVEx1v = [
+  ["1", "2", "3", "4", "5"],
+  ["I", "want", "it", "that", "way"],
+];
+const TestFile = [["You", "are", "my", "fire"]];
+export let data = new Map<string, string[][]>();
+data.set("way", CSVEx1v);
+data.set("fire", TestFile);
